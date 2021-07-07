ee2633c643c5b05f9693403aaf79833a98b874b4
ccheznoiu <86322968+ccheznoiu@users.noreply.github.com>
2021-07-07 13:56:52 -0400

    Update payment.go

--- payment.go
+++ payment.go
@@ -1,13 +1,5 @@
 package main
 
-/*
-An individual payment installment which is made on a payment plan. Many-to-one with debts.
-
-payment_plan_id (integer)
-amount (real)
-date (string) - ISO 8601 date of when this payment occurred.
-*/
-
 type paymentResponse struct {
 	//PaymentPlanID int    `json:"payment_plan_id"`
 	Amount USD    `json:"amount"`
